34f3434d57ba3e3d915f4ade6c74e8b4c535d773
Victoria Landron <viclandron@gmail.com>
2020-10-31 19:34:49 -0400

    wrote first product route to find all

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,6 +7,24 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock'
+    ],
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name']
+      }
+    ]
+  })
 });
 
 // get one product
