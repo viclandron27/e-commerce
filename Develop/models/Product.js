e8f17e0c01d41f1a3481e25cfa4ac105631a0ad1
Victoria Landron <viclandron@gmail.com>
2020-10-31 18:28:56 -0400

    added requirements for product model

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,34 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      //validate it's a decimal
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      //set as a default value of 10
+      //validate it is numeric
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
