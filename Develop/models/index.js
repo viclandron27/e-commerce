6f4505adf9f77e023492349071071565305e0a86
Victoria Landron <viclandron@gmail.com>
2020-10-31 19:05:44 -0400

    created associations between models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,13 +4,31 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+//create associations
+
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'productTag_tag',
+  foreignKey: 'tag_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'productTag_product',
+  foreignKey: 'product_id'
+})
 
 module.exports = {
   Product,
